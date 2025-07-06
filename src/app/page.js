69cb67c9d63a9c2f670388a8920997a9eb83ffef
apple <apple@apples-MacBook-Air.local>
2025-07-06 15:08:04 +0530

    feat: added SignIn SignUp with Supabase Auth

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,17 @@ import { FullButton, TextButton } from "@/components/buttons/buttons";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-const LandingPage = () => {
+const LandingPage = (path) => {
   const router = useRouter();
   const navToDash = () => {
-    router.push("/Dashboard");
+    router.push(path);
   };
   return (
-    <div className="h-screen w-screen fixed flex items-center justify-center">
-      <FullButton name={"Dashbord"} onClick={navToDash} />
+    <div className="h-screen w-screen fixed flex flex-col gap-20 items-center justify-center">
+      <FullButton name={"SignUp"} onClick={() => router.push("/SignUp/")} />
+      <button className="full-button" onClick={() => router.push("/SignIn/")}>
+        Sign In
+      </button>
     </div>
   );
 };
